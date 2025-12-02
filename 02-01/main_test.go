4b36b3169defd0f803c649eb56422d658038ba11
Piet de Vries <piet.de.vries@kabisa.nl>
2025-12-02 09:45:35 +0100

    Day 2 part 1

---
+++ 02-01/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetInvalidIdsInRange(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal([]int{11, 22}, GetInvalidIdsInRange(11, 22))
+	assert.Equal([]int{1188511885}, GetInvalidIdsInRange(1188511880, 1188511890))
+}
+
+func TestIsIdInvalid(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(IsIdInvalid(11))
+	assert.True(IsIdInvalid(1212))
+	assert.False(IsIdInvalid(121212))
+}
