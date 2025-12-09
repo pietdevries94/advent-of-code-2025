4198330f5d2c0517251e66dc4b612e168f849623
Piet de Vries <piet.de.vries@kabisa.nl>
2025-12-09 20:14:05 +0100

    Refactor day 9 part 2, again

--- 09-02/main.go
+++ 09-02/main.go
@@ -20,67 +20,22 @@ func main() {
 
 	inputScanner := bufio.NewScanner(inputFile)
 
-	allCoords := []Coords{}
-
-	matrixWidth := 0
-	matrixHeight := 0
+	allCoords := []*Coords{}
 
 	for inputScanner.Scan() {
 		line := inputScanner.Text()
 
 		current := StringToCoords(line)
-		allCoords = append(allCoords, current)
-
-		if current.x > matrixWidth {
-			matrixWidth = current.x
-		}
-		if current.y > matrixHeight {
-			matrixHeight = current.y
-		}
+		allCoords = append(allCoords, &current)
 	}
 
-	var prev *Coords
-
-	matrix := BuildMatrix(matrixWidth, matrixHeight)
-	for i, current := range allCoords {
-		fmt.Printf("Building Matrix: %v/%v\n", i, len(allCoords))
-		if i == 0 {
-			prev = &allCoords[len(allCoords)-1]
-		}
-
-		// draw a vertical line
-		if prev.x == current.x {
-			start := min(prev.y, current.y)
-			end := max(prev.y, current.y)
-
-			x := current.x
-			for y := start; y <= end; y++ {
-				matrix[x][y] = 1
-			}
-		}
-
-		// draw a horizontal line
-		if prev.y == current.y {
-			start := min(prev.x, current.x)
-			end := max(prev.x, current.x)
-
-			y := current.y
-			for x := start; x <= end; x++ {
-				matrix[x][y] = 1
-			}
-		}
-
-		prev = &current
-	}
-
-	allPreviousCords := []Coords{}
+	allPreviousCords := []*Coords{}
 	largestSize := 0
 
-	for i, current := range allCoords {
-		fmt.Printf("Finding: %v/%v\n", i, len(allCoords))
+	for _, current := range allCoords {
 		for _, c := range allPreviousCords {
 			size := CalculateSize(current, c)
-			if size > largestSize && SquareInMatrix(matrix, current, c) {
+			if size > largestSize && SquareInMatrix(allCoords, current, c) {
 				largestSize = size
 			}
 		}
@@ -99,7 +54,7 @@ func StringToCoords(str string) Coords {
 	return c
 }
 
-func CalculateSize(a, b Coords) int {
+func CalculateSize(a, b *Coords) int {
 	x := GetDiff(a.x, b.x)
 	y := GetDiff(a.y, b.y)
 
@@ -114,41 +69,36 @@ func GetDiff(a, b int) int {
 	return b - a + 1
 }
 
-func BuildMatrix(matrixWidth, matrixHeight int) [][]int {
-	res := make([][]int, matrixWidth+1)
-	for i := range matrixWidth + 1 {
-		res[i] = make([]int, matrixHeight+1)
-	}
-	return res
-}
+func SquareInMatrix(allCoords []*Coords, a, b *Coords) bool {
+	minX := min(a.x, b.x)
+	maxX := max(a.x, b.x)
+	minY := min(a.y, b.y)
+	maxY := max(a.y, b.y)
 
-func SquareInMatrix(matrix [][]int, a, b Coords) bool {
-	// I make the educated guess that if the diff between the x's or y's is less than 3, it won't be the answer
-	// This simplifies the implementation
-	if GetDiff(a.x, b.x) < 3 || GetDiff(a.y, b.y) < 3 {
-		return false
-	}
-
-	minX := min(a.x, b.x) + 1
-	maxX := max(a.x, b.x) - 1
-	minY := min(a.y, b.y) + 1
-	maxY := max(a.y, b.y) - 1
-
-	for y := minY; y <= maxY; y++ {
-		if matrix[minX][y] == 1 {
-			return false
+	var prev *Coords
+	for i, c := range allCoords {
+		if i == 0 {
+			prev = allCoords[len(allCoords)-1]
+		} else {
+			prev = allCoords[i-1]
 		}
-		if matrix[maxX][y] == 1 {
-			return false
-		}
-	}
 
-	for x := minX; x <= maxX; x++ {
-		if matrix[x][minY] == 1 {
-			return false
+		// if the x is the same, we need to check if the a is in the y range of the line
+		if prev.x == c.x && c.x > minX && c.x < maxX {
+			minYLine := min(prev.y, c.y)
+			maxYLine := max(prev.y, c.y)
+			if maxY > minYLine && minY < maxYLine {
+				return false
+			}
 		}
-		if matrix[x][maxY] == 1 {
-			return false
+
+		// the same for y
+		if prev.y == c.y && c.y > minY && c.y < maxY {
+			minXLine := min(prev.x, c.x)
+			maxXLine := max(prev.x, c.x)
+			if maxX > minXLine && minX < maxXLine {
+				return false
+			}
 		}
 	}
 	return true
